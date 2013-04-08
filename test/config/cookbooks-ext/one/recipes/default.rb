afa4a88a4c73eb3dcdaa3acb1904f2ebaaf408af
Yamashita Yuu <yamashita@geishatokyo.com>
2013-04-08 20:08:16 +0900

    use data_bag values in recipes

--- test/config/cookbooks-ext/one/recipes/default.rb
+++ test/config/cookbooks-ext/one/recipes/default.rb
@@ -2,6 +2,7 @@ file "/tmp/one" do
   owner "root"
   group "root"
   mode "0644"
+  content data_bag_item("one", "data")["value"]
 end
 
 # vim:set ft=ruby sw=2 ts=2 :
--- test/config/cookbooks/foo/recipes/default.rb
+++ test/config/cookbooks/foo/recipes/default.rb
@@ -2,6 +2,7 @@ file "/tmp/foo" do
   owner "root"
   group "root"
   mode "0644"
+  content data_bag_item("foo", "data")["value"]
 end
 
 # vim:set ft=ruby sw=2 ts=2 :
